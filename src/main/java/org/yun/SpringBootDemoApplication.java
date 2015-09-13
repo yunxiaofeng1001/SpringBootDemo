8996a3e20b835dd1ccad9e334b4899321635af6c
yunxiaofeng <yunxiaofeng@yintai.com>
2015-09-13 16:31:01 +0800

    init develop

--- src/main/java/org/yun/SpringBootDemoApplication.java
+++ src/main/java/org/yun/SpringBootDemoApplication.java
@@ -8,5 +8,6 @@ public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
+        System.out.println("init");
     }
 }
